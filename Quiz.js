3d169a287d0f14d81cf1d21996a72398c16ff9be
demiruns <demirunsewe@gmail.com>
2026-04-19 17:35:44 +0200

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,49 @@
+var score = 0
+const signs = ['-', '+', '*', '**'];
+var correct_answer = 0
+Question()
+
+function Shuffle(list){
+    for (var i in list){
+        randswap = Math.floor(Math.random() * list.length)
+        list[i],list[randswap]=list[randswap],list[i]
+    }
+}
+
+function Question(){
+    const answers = []
+    randno1 = Math.floor(Math.random() * 4) + 5;
+    randno2 = Math.floor(Math.random() * 4) + 2;
+    randno = Math.floor(Math.random() * 4);
+    console.log(randno)
+    if (signs[randno]=="-"){
+        correct_answer=randno1 - randno2
+    }
+    if (signs[randno]=="+"){
+        correct_answer=randno1 + randno2
+    }
+    if (signs[randno]=="*"){
+        correct_answer=randno1 * randno2
+    }
+    if (signs[randno]=="**"){
+        correct_answer=randno1 ** randno2
+    }
+    answers.push(randno1 - randno2)
+    answers.push(randno1 + randno2)
+    answers.push(randno1 * randno2)
+    answers.push(randno1 ** randno2)
+    Shuffle(answers)
+    console.log(answers)
+    document.getElementById("No1").innerHTML = randno1;
+    document.getElementById("No2").innerHTML = randno2;
+    document.getElementById("Math").innerHTML = signs[randno];
+    document.getElementById("Button1").innerHTML = answers[0];
+    document.getElementById("Button2").innerHTML = answers[1];
+    document.getElementById("Button3").innerHTML = answers[2];
+    document.getElementById("Button4").innerHTML = answers[3];
+}
+
+function Button(answer){
+    
+    document.getElementById("Cscore").innerHTML=score
+    }
